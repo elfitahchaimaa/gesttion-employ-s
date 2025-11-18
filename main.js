b001ccb6ff1bc0de31aa892c0fb1d7881db40eb0
elfitahchaimaa <issambadri0>
2025-11-18 20:46:33 +0100

    ajout d un fichier js pour mainbar

---
+++ main.js
@@ -0,0 +1,17 @@
+const accessrules={
+    "reception":["Réceptioniste","Manager"],
+    "salle-de-serveurs":["Technicien-IT","Manager"],
+    "Salle-de-sécurité": ["Agent de sécurité", "Manager"],
+    "Salle-darchives": ["Manager"],
+    //ce sont les zones libres
+    "Salle-du-personnel": "all",
+    "conference": "all",
+    "div3": "all"
+}
+function hasAccess(Role,zone){
+    const rule=accessrules[zone];
+    if(rule==="all"){
+        return true;
+    }
+    return rule.includes(Role);
+}
